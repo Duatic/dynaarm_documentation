214950f2e2fbecfb2610b718918b98ca0a29425d
Lennart Nachtigall <mail@firesurfer.de>
2024-11-22 13:05:26 +0100

    Init

---
+++ source/conf.py
@@ -0,0 +1,39 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'DynaArm Software Documentation'
+copyright = '2024, Duatic AG'
+author = 'Lennart Nachtigall'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+
+html_context = {
+    "display_github": True,
+    "github_user": "Duatic",
+    "github_repo": "dynaarm_software_documentation",
+    "favicon": "favicon.ico",
+    "logo": "logo.png"
+}
+
+html_favicon = "_static/logo.png"
+html_logo = "_static/logo.png"
