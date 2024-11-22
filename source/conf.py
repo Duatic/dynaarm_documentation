e31d9076ef3b2ce664cd4029fa83e8e2e3b5252d
Lennart Nachtigall <mail@firesurfer.de>
2024-11-22 13:31:21 +0100

    added repos.list and clone subrepositories

--- source/conf.py
+++ source/conf.py
@@ -13,7 +13,12 @@ author = 'Lennart Nachtigall'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx_copybutton",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.todo",
+    "sphinx_rtd_theme",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -31,6 +36,8 @@ html_context = {
     "display_github": True,
     "github_user": "Duatic",
     "github_repo": "dynaarm_software_documentation",
+    "github_version": "main/",
+    "conf_py_path": "/source/",
     "favicon": "favicon.ico",
     "logo": "logo.png"
 }
